355033e4e493ac0518382f25433dbe8c71956cf4
Ben Houston <BenjaminAHouston@gmail.com>
2018-11-18 17:18:37 -0500

    Refactored the code so apiRoutes wouldn't need to require anything AND could handle everything internally

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,38 +1,41 @@
-const path = require('path');
-const fs = require('fs');
+let router = function(app, path, fs) {
+    app.get("/api/friends", (req, res) => {
+        fs.readFile(path.join(__dirname, "../data/friends.js"), 'utf8', (error, result) => {
+            if (error) throw error;
+            res.json(JSON.parse(result));
+        });
+    });
 
-let router = function(req, res) {
-    fs.readFile(path.join(__dirname, "../data/friends.js"), 'utf8', (err, data) => {
-        if (err) throw err;
-        let array = JSON.parse(data);
-        if (req.method === "GET") {
-            res.json(array);
-        } else if (req.method === "POST") {
-            let diff = Infinity;
-            let index = -1
-            let submittedSum = 0;
-            req.body.scores.forEach(num => {
-                submittedSum += parseInt(num);
-            });
-
+    app.post("/api/friends", (req, res) => {
+        fs.readFile(path.join(__dirname, "../data/friends.js"), 'utf8', (error, result) => {
+            if (error) throw error;
+            let sum =  calcSum(req.body.scores);
+            let array = JSON.parse(result);
+            let lowest = Infinity;
+            let index = -1;
             array.forEach((element, i) => {
-                let sum = 0;
-                element.scores.forEach(num => {
-                    sum += parseInt(num);
-                });
-                if (Math.abs(sum - submittedSum) < diff) {
-                    diff = Math.abs(sum - submittedSum);
+                let tempSum = calcSum(element.scores);
+                if (Math.abs(sum - tempSum) < lowest) {
                     index = i;
+                    lowest = tempSum;
                 }
             });
-
+            res.json(array[index]);
             array.push(req.body);
-            fs.writeFile(path.join(__dirname, "../data/friends.js"), JSON.stringify(array, null, 2), 'utf8', err => {
-                if (err) throw err;
-                res.end(JSON.stringify(array[index]));
-            });
-        }
+            // fs.writeFile(path.join(__dirname, "../data/friends.js"), JSON.stringify(array, null, 2), 'utf8', err => {
+            //     if (err) throw err;
+            // });
+        });
+        
     });
+}
+
+function calcSum(array) {
+    let sum = 0;
+    array.forEach(element => {
+        sum += parseInt(element);
+    });
+    return sum;
 }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
-const htmlRoutes = require('./app/routing/htmlRoutes');
-const apiRoutes = require('./app/routing/apiRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const htmlRoutes = require('./app/routing/htmlRoutes');
+const apiRoutes = require('./app/routing/apiRoutes')(app, path, fs);
+
 app.get('/:route?', htmlRoutes);
-app.get('/api/:route', apiRoutes);
-app.post('/api/:route', apiRoutes);
 
 app.listen(8080);
