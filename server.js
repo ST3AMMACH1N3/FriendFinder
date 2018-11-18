d2abb3443d889fa2d4a79f9e15d6189be554ae3e
Ben Houston <BenjaminAHouston@gmail.com>
2018-11-18 18:08:39 -0500

    added PORT so the server can start on whatever port from heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
+const PORT = process.env.PORT || 8080
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -9,4 +10,4 @@ app.use(express.json());
 const apiRoutes = require('./app/routing/apiRoutes')(app, path, fs);
 const htmlRoutes = require('./app/routing/htmlRoutes')(app, path);
 
-app.listen(8080);
+app.listen(PORT);
